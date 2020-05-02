0a4b3ac2938e09291af225996d46e81a10d95866
Ajinkya Bapat <ajinkyabapat1@gmail.com>
2020-05-02 16:08:58 +0530

    project setup done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'recipies',
+    loadChildren: () => import('./recipies/recipies.module').then( m => m.RecipiesPageModule)
+  },
 ];
 
 @NgModule({
