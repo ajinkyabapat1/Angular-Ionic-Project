946369961c68338a89b3173cb7e5bd7ae77ebdd2
Ajinkya Bapat <ajinkyabapat1@gmail.com>
2020-05-02 17:02:35 +0530

    added recipie page

--- src/app/recipies/recipies.page.ts
+++ src/app/recipies/recipies.page.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Recipie } from "./recipie.model";
 
 @Component({
-  selector: 'app-recipies',
-  templateUrl: './recipies.page.html',
-  styleUrls: ['./recipies.page.scss'],
+  selector: "app-recipies",
+  templateUrl: "./recipies.page.html",
+  styleUrls: ["./recipies.page.scss"],
 })
 export class RecipiesPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
+  recipies: Recipie[] = [
+    {
+      id: "R1",
+      title: "Paneer Butter Masal",
+      imageUrl: "assets/paneer.jpg",
+      ingrediants: ["Paneer", "Butter"],
+    },
+    {
+      id: "R2",
+      title: "Gulab Jamun",
+      imageUrl: "assets/jamun.jpg",
+      ingrediants: ["Khava", "Ghee"],
+    },
 
+    {
+      id: "R3",
+      title: "Biryani",
+      imageUrl: "assets/biryani.jpg",
+      ingrediants: ["Rice", "Biryani Masala", "Paneer"],
+    },
+  ];
 }
